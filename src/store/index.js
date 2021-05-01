25790240572ddc3ea1932bb6f507371ca0bfe251
Carlito <carlorody1000@gmail.com>
2021-05-01 17:51:46 -0500

    WIP

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    msg: "Estudia Oe",
+    nombre: "Rody",
+    apellido: "Castillo",
   },
-  mutations: {
+  mutations: {},
+  actions: {},
+  getters: {
+    mesaje(state) {
+      return state.msg;
+    },
+    completedName(state) {
+      return `${state.nombre} ${state.apellido}`;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+});
